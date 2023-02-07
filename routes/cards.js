b890646d4a815a4f4439d243add4132eec2c504a
Peter Sidorov <kripns06@gmail.com>
2023-02-07 21:56:14 +1200

    feat/ implement requests validation in card routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-// import { celebrate, Joi } from 'celebrate';
+import { celebrate, Joi } from 'celebrate';
 import {
   getAllCards, createCard, deleteCard, likeCard, dislikeCard,
 } from '../controllers/cards.js';
@@ -8,9 +8,46 @@ import {
 const router = express.Router();
 
 router.get('/', getAllCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi
+      .string()
+      .min(2)
+      .max(30)
+      .required(),
+    link: Joi
+      .string()
+      .uri()
+      .required(),
+  }),
+}), createCard);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi
+      .string()
+      .alphanum()
+      .length(24),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi
+      .string()
+      .alphanum()
+      .length(24),
+  }),
+}), likeCard);
+
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi
+      .string()
+      .alphanum()
+      .length(24),
+  }),
+}), dislikeCard);
 
 export default router;
